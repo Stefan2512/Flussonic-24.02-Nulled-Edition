5b3e8004895a7e630930b1dde8c58a3cf88b5e8c
Sony2512 <stefan@outlook.ie>
2025-07-14 22:37:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,67 +1,59 @@
 #!/bin/bash
 
-set -e
-set -u
-export LANG=C
-export PATH=/bin:/sbin:/usr/bin:/usr/sbin
-
 bold=$(tput bold)
 underline=$(tput smul)
-info=$(tput setaf 2)
-error=$(tput setaf 160)
 warn=$(tput setaf 214)
 reset=$(tput sgr0)
 
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root"
+   echo "This script must be run as root" 
    exit 1
 fi
 
-echo "    ________                            _         _   __      ____         __"
-echo "   / ____/ /_  ________________  ____  (_)____   / | / /_  __/ / /__  ____/ /"
-echo "  / /_  / / / / / ___/ ___/ __ \/ __ \/ / ___/  /  |/ / / / / / / _ \/ __  / "
-echo " / __/ / / /_/ (__  |__  ) /_/ / / / / / /__   / /|  / /_/ / / /  __/ /_/ /  "
-echo "/_/   /_/\__,_/____/____/\____/_/ /_/_/\___/  /_/ |_/\__,_/_/_/\___/\__,_/   "
-echo "  "
 echo "${underline}${warn}${bold}--= Flussonic 24.02 Nulled Private Release =--${reset}"
-echo "  "
 
-# Instalare Flussonic oficial
-curl -sSf https://flussonic.com/public/install.sh | sh
+distro=debian
+echo "Distro: $distro"
 
-# Instalare pachet .deb nulled din directorul resources
-cd resources/
-dpkg -i *.deb
+# Update and install Flussonic
+apt-get update
+curl -sSf http://apt.flussonic.com/binary/gpg.key > /etc/apt/trusted.gpg.d/flussonic.gpg
+echo "deb http://apt.flussonic.com binary/" > /etc/apt/sources.list.d/flussonic.list
+apt-get update
+apt-get -y install flussonic
 
-# Config implicit
+# Config default
 cat > /etc/flussonic/flussonic.conf <<EOF
 http 80;
 rtmp 1935;
 srt 1234;
-pulsedb /var/lib/flussonic;
-session_log /var/lib/flussonic;
-edit_auth admin admin;
-
 iptv;
+edit_auth admin admin;
 EOF
 
-# Pornire serviciu
-service flussonic start || true
-/etc/init.d/flussonic restart || true
+# Creează systemd service
+cat > /etc/systemd/system/flussonic.service <<EOF
+[Unit]
+Description=Flussonic Streaming Server
+After=network.target
 
-# Info server
+[Service]
+ExecStart=/opt/flussonic/flussonic
+Restart=always
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reexec
+systemctl daemon-reload
+systemctl enable flussonic
+systemctl start flussonic
+
+# Afiseaza info
 local_ip=$(hostname -I | awk '{print $1}')
-http_port=80
-rtmp_port=1935
-srt_port=1234
-
-echo "  "
-echo "${warn}${bold}Installation Complete!${reset}"
-echo "${warn}${bold}Local IP: $local_ip${reset}"
-echo "${warn}${bold}HTTP Port: $http_port${reset}"
-echo "${warn}${bold}RTMP Port: $rtmp_port${reset}"
-echo "${warn}${bold}SRT Port: $srt_port${reset}"
-echo "${warn}${bold}Web Panel: http://$local_ip:$http_port${reset}"
-echo "${warn}${bold}Username: admin${reset}"
-echo "${warn}${bold}Password: admin${reset}"
-echo "${warn}${bold}Copy License Key: l4|AbOFvyPq7piW0ub_MfFUL2|r6BzpmVPpjgKpn9IunpFp6lLbCZOp3${reset}"
+echo ""
+echo "${warn}${bold}Flussonic running at: http://$local_ip:80${reset}"
+echo "${warn}${bold}Username: admin | Password: admin${reset}"
+echo "${warn}${bold}Crack License: l4|AbOFvyPq7piW0ub_MfFUL2|r6BzpmVPpjgKpn9IunpFp6lLbCZOp3${reset}"
